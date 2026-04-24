672b260a9d25bc475a157e889c5c39ec81886d77
leluongnghia <leluongnghia@gmail.com>
2026-04-24 13:00:45 +0700

    feat: bump version to 1.6.10 and clean up temp files

--- vps-manager/do_commit_1.6.9.bat
+++
@@ -1,3 +0,0 @@
-git add -A
-git commit -m "feat: v1.6.9 - Doi socket path cua Valkey/Redis sang /var/run de an toan khi reboot"
-git push
---
+++ vps-manager/do_commit_1610.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "feat: bump version to 1.6.10 and clean up temp files"
+git push
